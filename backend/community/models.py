a83817242eac8331d39fe638a6057ba6cf6aa30b
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-11-10 15:30:24 +0300

    updated models for cleaner look on properties

--- backend/community/models.py
+++ backend/community/models.py
@@ -5,23 +5,35 @@ from django.utils import timezone
 
 
 class DataFieldType(models.Model):
-    name = models.CharField(max_length=30)
-    type = models.CharField(max_length=30)
+    name = models.CharField(
+        max_length=30
+    )
+    type = models.CharField(
+        max_length=30
+    )
 
     def __str__(self):
         return self.name
 
 
 class WikidataItem(models.Model):
-    item = models.CharField(max_length=25)
-    label = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    item = models.CharField(
+        max_length=25
+    )
+    label = models.CharField(
+        max_length=100
+    )
+    description = models.CharField(
+        max_length=200
+    )
 
     def __str__(self):
         return self.label
 
 class DataField(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(
+        max_length=50
+    )
     # todo i should either use enumeration on type or open a new table for type
     type = models.OneToOneField(
         DataFieldType,
@@ -41,9 +53,13 @@ class DataField(models.Model):
 
 
 class DataType(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(
+        max_length=100
+    )
     # todo update class diagram for this property
-    body = models.TextField(max_length=2000)
+    body = models.TextField(
+        max_length=2000
+    )
     data_fields = models.ManyToManyField(
         DataField
     )

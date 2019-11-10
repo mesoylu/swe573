a582dfff88c14713c9f6dfdc8e06c90893486673
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-11-10 18:47:43 +0300

    created User, Community, Post and FieldValue models and migrations

--- backend/community/admin.py
+++ backend/community/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import DataFieldType, DataField, WikidataItem, DataType
+from .models import DataFieldType, DataField, WikidataItem, DataType, User, Community, Post, FieldValue
 
 admin.site.register(DataFieldType)
 
@@ -10,3 +10,11 @@ admin.site.register(WikidataItem)
 admin.site.register(DataField)
 
 admin.site.register(DataType)
+
+admin.site.register(User)
+
+admin.site.register(Community)
+
+admin.site.register(Post)
+
+admin.site.register(FieldValue)
--- backend/community/models.py
+++ backend/community/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
 
 
 class DataFieldType(models.Model):
@@ -30,6 +31,68 @@ class WikidataItem(models.Model):
     def __str__(self):
         return self.label
 
+
+class User(models.Model):
+    username = models.CharField(
+        max_length=50,
+        unique=True
+    )
+    password = models.CharField(
+        max_length=32
+    )
+    email = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    image_path = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+    date_registered = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Community(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    description = models.TextField(
+        max_length=300
+    )
+    # todo should we proceed with reddit style naming conventions
+    url = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    # todo research on audio video image upload on django
+    image_path = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name="creator"
+    )
+    members = models.ManyToManyField(
+        User,
+        related_name="member"
+    )
+    date_created = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class DataField(models.Model):
     name = models.CharField(
         max_length=50
@@ -47,6 +110,12 @@ class DataField(models.Model):
         blank=True,
         on_delete=models.PROTECT
     )
+    community = models.ForeignKey(
+        Community,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.name
@@ -61,19 +130,63 @@ class DataType(models.Model):
         max_length=2000
     )
     data_fields = models.ManyToManyField(
-        DataField
+        DataField,
+        # todo it is hard to give callable to limit_choices_to property
+        # limit_choices_to={'community': 1},
+    )
+    community = models.ForeignKey(
+        Community,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
     )
 
     def __str__(self):
         return self.title
 
+
+class Post(models.Model):
+    data_type = models.ForeignKey(
+        DataType,
+        on_delete=models.PROTECT
+    )
+    date_created = models.DateTimeField(
+        auto_now=True
+    )
+    # todo these two should come from vote table, right now it is a placeholder
+    upvote_count = models.IntegerField()
+    downvote_count = models.IntegerField()
+
+    def __str__(self):
+        return self.data_type.title + " : " + str(self.pk)
+
+
+# todo this class will be added to diagram
+class FieldValue(models.Model):
+    data_field = models.ForeignKey(
+        DataField,
+        on_delete=models.PROTECT
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.PROTECT
+    )
+    value = models.CharField(
+        max_length=200
+    )
+
+    def __str__(self):
+        return self.data_field.name + " : " + self.value
+
+
+
 #
 #
-#class User(models.Model):
+# class User(models.Model):
 #    email = models.CharField(max_length=200)
 #    password = models.CharField(max_length=200)
 #    username = models.CharField(max_length=80)
 #    date_registered = models.DateTimeField(auto_now=True)
 #    imagePath = models.UUIDField()
 #
-#class Post(models.Model):
+# class Post(models.Model):

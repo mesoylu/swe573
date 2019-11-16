fdde3b00f4e3a6215a49b1c5fd74e7dbce0a8e51
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-11-16 20:18:24 +0300

    fix on rename file function

--- backend/community/models.py
+++ backend/community/models.py
@@ -13,13 +13,7 @@ from uuid import uuid4
 def path_and_rename(path):
     def wrapper(instance, filename):
         ext = filename.split('.')[-1]
-        # get filename
-        if instance.pk:
-            filename = '{}.{}'.format(instance.pk, ext)
-        else:
-            # set filename as random string
-            filename = '{}.{}'.format(uuid4().hex, ext)
-        # return the whole path to the file
+        filename = '{}.{}'.format(uuid4().hex, ext)
         return os.path.join(path, filename)
     return wrapper
 

bbaf1ceeb332fdf9db6fb2bd86d5979abcf9c28d
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-11-20 14:24:57 +0300

    updated urlpatterns to cover all methods

--- backend/community/urls.py
+++ backend/community/urls.py
@@ -4,22 +4,80 @@ from django.conf.urls.static import static
 
 from . import views
 
-# todo https://docs.djangoproject.com/en/2.2/howto/static-files/ it is not suitable on prod
+# urlpatterns = \
+#     [
+#         path('login', views.login),
+#         path('c/<name>/', include([
+#             path('', views.getCommunity),
+#             path('members/', views.getCommunityMembers),
+#             path('datatypes/', views.getCommunityDataTypes),
+#             path('datafields/', views.getCommunityDataFields),
+#             path('posts/', views.getCommunityPosts),
+#         ])),
+#         path('u/<username>/', include([
+#             path('', views.getUser),
+#         ])),
+#         path('p/<url>/', include([
+#             path('', views.getPost),
+#         ])),
+#         path('wiki', views.wiki)
+#     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = \
     [
-        path('login', views.login),
-        path('c/<name>/', include([
-            path('', views.getCommunity),
-            path('members/', views.getCommunityMembers),
-            path('datatypes/', views.getCommunityDataTypes),
-            path('datafields/', views.getCommunityDataFields),
-            path('posts/', views.getCommunityPosts),
+        # todo homepage
+        # path('', views.index),
+        # todo community related views
+        path('c/', include([
+            # todo GET getAllCommunities, POST createCommunity
+            # path('', views.Communities),
+            path('<name>', include([
+                # todo GET getCommunity, PATCH updateCommunity, DELETE deleteCommunity
+                # path('', views.Community),
+                # todo GET getMembers, POST subscribe
+                # path('members/', views.CommunityMembers),
+                # todo GET getDataTypes, POST createDataType
+                # path('datatypes/', views.getCommunityDataTypes),
+                # todo GET getDataFields, POST createDataField
+                # path('datafields/', views.getCommunityDataFields),
+                # todo GET getCommunityPosts
+                # path('posts/', views.getCommunityPosts)
+            ]))
         ])),
-        path('u/<username>/', include([
-            path('', views.getUser),
+        # todo user related views
+        path('u/', include([
+            # todo GET getAllUsers
+            # path('', views.Users),
+            # todo GET loginform?, POST login
+            # path('login', views.Login),
+            # todo GET signupform?, POST signup
+            # path('signup', views.Signup),
+            path('<username>', include([
+                # todo GET getUser, PATCH updateUser, DELETE deleteUser
+                # path('', views.User),
+                # todo GET getDataTypes, POST createDataType
+                # path('datatypes/', views.getUserDataTypes),
+                # todo GET getDataFields, POST createDataField
+                # path('datafields/', views.getUserDataFields),
+                # todo GET getCommunityPosts
+                # path('posts/', views.getUserPosts)
+                # todo GET lastVotedByUser
+                # path('votes/', views.getUserVotes)
+            ])),
         ])),
-        path('p/<url>/', include([
-            path('', views.getPost),
+        # todo post related views
+        path('p/', include([
+            # todo GET getAllPosts
+            # path('', views.Posts),
+            path('<url>', include([
+                # todo GET getPost, PATCH updatePost, DELETE deletePost
+                # path('', views.Post),
+                # todo POST vote, PATCH updateVote, DELETE deleteVote
+                # path('vote', views.Vote),
+            ]))
         ])),
+        # todo wikidata related views
         path('wiki', views.wiki)
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+    # todo https://docs.djangoproject.com/en/2.2/howto/static-files/ it is not suitable on prod
+

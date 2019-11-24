9796ced549e903d8b9737565b612ba434974b1e2
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-11-24 13:40:55 +0300

    removed todo comment

--- backend/community/urls.py
+++ backend/community/urls.py
@@ -63,7 +63,6 @@ urlpatterns = \
         path('p/', include([
             path('', views.PostViews.index),
             path('<url>/', include([
-                # todo GET getPost, PATCH updatePost, DELETE deletePost
                 path('', views.PostViews.post),
                 # todo POST vote, PATCH updateVote, DELETE deleteVote
                 # path('vote', views.Vote),

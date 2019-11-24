02fa1678b687abf05a2e63fe69fb66907630b5fc
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-11-24 19:17:03 +0300

    updated model DataType added is_archived boolean field

--- backend/community/models.py
+++ backend/community/models.py
@@ -234,6 +234,9 @@ class DataType(models.Model):
     date_created = models.DateTimeField(
         auto_now=True
     )
+    is_archived = models.BooleanField(
+        default=False
+    )
 
     def __str__(self):
         return self.name

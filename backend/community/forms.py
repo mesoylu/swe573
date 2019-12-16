68f9202eb1285c9c5a63d52f4a7275873f7a8f05
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-12-16 17:41:50 +0300

    created user related forms, will not use them 

--- backend/community/forms.py
+++ backend/community/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+
+from django import forms
+
+class UserCreateForm(UserCreationForm):
+    email = forms.CharField(required=True)
+    image = forms.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = ("username", "email","image", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.image = self.cleaned_data["image"]
+        if commit:
+            user.save()
+        return user
 
 
 class CommunityForm(forms.ModelForm):
@@ -8,3 +27,8 @@ class CommunityForm(forms.ModelForm):
         model = Community
         fields = '__all__'
 
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','password','email','image']

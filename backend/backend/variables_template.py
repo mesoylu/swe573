882f0b3d22c9316c2c8b952a4c47efb0a0244a6f
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2019-12-24 15:56:39 +0300

    added variables_template.py

---
+++ backend/backend/variables_template.py
@@ -0,0 +1,13 @@
+# settings.py imports variables.py for the variables below
+# copy and rename this file as variables.py and add database credentials and static file paths
+db_name = ''
+db_host = ''
+db_user = ''
+db_password = ''
+db_port = ''
+
+static_root = ''
+staticfiles_dirs = (
+    '',
+)
+media_root = ''
